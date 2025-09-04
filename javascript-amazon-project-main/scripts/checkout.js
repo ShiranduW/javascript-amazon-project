da34392faaa4767de5ddaa81e672e917aabc2cad
Shirandu Warnakulasuriya <shirandushirandu@gmail.com>
2025-09-04 21:28:55 +0530

    Fix price formatting and radio selectors

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,8 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./util/money.js";
 
-let cartSummaryHtml = '';
+let cartSummaryHtml = "";
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -13,7 +14,7 @@ cart.forEach((cartItem) => {
     }
   });
 
-  cartSummaryHtml += `<div class="cart-item-container">
+  let html = `<div class="cart-item-container">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -27,7 +28,7 @@ cart.forEach((cartItem) => {
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${matchingProduct.priceCents / 100}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -51,7 +52,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -64,7 +65,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -77,7 +78,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -92,7 +93,7 @@ cart.forEach((cartItem) => {
           </div>
     `;
 
+    cartSummaryHtml += html;
   console.log(cartSummaryHtml);
-  document.querySelector(".js-order-summary")
-   .innerHTML = cartSummaryHtml;
+  document.querySelector(".js-order-summary").innerHTML = cartSummaryHtml;
 });
